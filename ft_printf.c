61ac704d0a725a89936f7b7fb36844be8091c5ea
Fernanda Eschallots <feschall@il-i5.msk.21-school.ru>
2021-03-04 21:41:49 +0300

    v.2

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: feschall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 13:34:09 by feschall          #+#    #+#             */
-/*   Updated: 2021/03/04 14:04:00 by feschall         ###   ########.fr       */
+/*   Updated: 2021/03/04 21:40:02 by feschall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,62 @@ int ft_strlen(const char *s)
    int len;
 
    len = 0;
-   while(*s++ != '\0')
+   while(s[len])
       len++;
    return (len);
 }
+
+int ft_putstr(char *str, int len, t_struct *check)
+{	
+	while (*str != '\0' && len-- > 0)
+		check->rezult += write(1, str++, 1);
+	return (check->rezult);
+}
+
+void ft_printf_s(char *str,t_struct *check)
+	{
+		int len_t;
+
+		check->len_s = ft_strlen(str);
+
+		if (check->width >= check->len_s && check->precision && !(check->flag & F_MINS))// Заполняем |<-
+		{
+			if (check->precision > 0)
+				check->len_s = check->precision;
+			len_t = (check->width - check->len_s); 
+				while (len_t-- > 0)
+					check->rezult += write(1, " ", 1);	// Заполняем пробелами левый край
+			ft_putstr(str, check->len_s, check);	//Дозаполняем строку до конца
+		}
+		else if (check->width >= check->len_s && check->precision && (check->flag & F_MINS)) // Заполняем ->|
+		{
+			if (check->precision > 0)
+				check->len_s = check->precision;
+			ft_putstr(str, check->len_s, check);	//Заполняем строку указанной точности
+			len_t = (check->width - check->len_s); 
+				while (len_t-- > 0)
+					check->rezult += write(1, " ", 1);	// Заполняем пробелами левый край
+		}
+	}
+
 
 int ft_printf(const char *str, ...)
 {
 	t_struct *check;
 	
-	check->rezult = 0;
 	check->flag = F_NULL;
+	check->rezult = 0;
 	check->width = 0;
-	check->dot = 0;
+	check->precision = 0;
 	check->len_s = 0;
 
    va_list ap;
    va_start(ap, str);
    while(*str != '\0')
    {
-	   if (!(*str == '%'))
+	   if (*str != '%')
 	   {
          check->rezult+= write(1, str++, 1);
-		 check->len_s ++;
 	   }
       else
 	  {
@@ -54,60 +87,55 @@ int ft_printf(const char *str, ...)
 		{
 			check->flag = check->flag | F_ZERO;
 			if(check->flag & F_MINS)
-				check->flag = F_ZERO;	//Проверить работу условия, что если есть минус, то 0 игнорируется!
+				check->flag = F_MINS;	//Проверить работу условия, что если есть минус, то 0 игнорируется!
 			check->rezult++;
 			str++;
 		}
 		if (*str == '*')	//Проверка ширину указанную в списке аргументов
 			check->width = va_arg(ap, int);
-		// else
-		while ('0' <= *str && *str <= '9') //Проверяем на числовое значение ширины. Вероятно надо использовать итоа!
+			while ('0' <= *str && *str <= '9') //Проверяем на числовое значение ширины. Вероятно надо использовать итоа!
 			{
 				check->width = check->width * 10 + (*str - '0');
 				check->rezult++;
 				str++;
 			}
-		if (*str == '.')	//Проверка точности
+				if (*str == '.')	//Проверка точности
 		{
 			if(*++str == '*' || ('0' <= *str && *str <= '9'))	//Если следующее значение * или цифра, то точность есть!
 			{
 				check->flag= check->flag | F_DOT;
 				if (*str == '*') //Проверка точность указанную в списке аргументов
 				{
-					check->dot = va_arg(ap, int);
+					check->precision = va_arg(ap, int);
 					check->rezult++;
 					str++;
 				}
 				// else
 				while ('0' <= *str && *str <= '9') //Проверяем на числовое значение точности
 					{
-						check->dot = check->dot * 10 + (*str - '0');
+						check->precision = check->precision * 10 + (*str - '0');
 						check->rezult++;
 						str++;
 					}
 			}	
 			check->rezult++;
 		}
-		if (*str == 's' || *str == 'p' || *str == 'd' || *str == 'i' || *str == 'u' || *str == 'x' || *str == 'X')	//Проверка типов
+		if (*str == 's')
 		{
+			ft_printf_s(va_arg(ap, char *), check);
 			str++;
 		}
-	  }
-
+	}
    }
    va_end(ap);
-
-   printf("1 %u\n", check->rezult);
-   printf("2 %d\n", check->flag);
-   printf("3 %c\n", check->type);
-   printf("4 %d\n", check->dot);
-   
    return (check->rezult);
 }
 
 int main()
 {
-	printf("test2 text%-012.9s\n");
-	ft_printf("test2 text%-012.9s\n");
+	printf("test1 text%-10.5s\n", "1234567890");
+	printf("test2 text%10.5s\n", "1234567890");
+	ft_printf("test3 text%-10.5s\n", "1234567890");
+	ft_printf("test4 text%10.5s\n", "1234567");
    return (0);
 }
--- ft_printf_type.c
+++ ft_printf_type.c
@@ -1,35 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf_type.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: feschall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/02 13:37:24 by feschall          #+#    #+#             */
-/*   Updated: 2021/03/04 21:40:56 by feschall         ###   ########.fr       */
+/*   Created: 2021/03/04 21:26:27 by feschall          #+#    #+#             */
+/*   Updated: 2021/03/04 21:40:48 by feschall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdio.h> // Delete!
-# include <stdarg.h>
-# include <unistd.h>
 
-# define F_NULL	0b00000000
-# define F_MINS	0b00000001
-# define F_ZERO	0b00000010
-# define F_DOT	0b00000100
-
-typedef struct s_struct
+void ft_printf_type()
 {
-    int 			len_s;
-	unsigned int	rezult;
-	unsigned char	flag;
-	int				width;
-	int				precision;
-	char			type;
-}				t_struct;
-
-#endif
+    
+}
